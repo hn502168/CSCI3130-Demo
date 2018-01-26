899fe0e716b486cec1c009035d69cdf350262120
hn502168 <35535038+hn502168@users.noreply.github.com>
2018-01-26 12:19:55 -0400

    Add button and textfile to change the text

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
+    private TextView textview;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button1);
+        editText = (EditText) findViewById(R.id.editText);
+        textview = (TextView) findViewById(R.id.textview);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textview.setText(editText.getText());
+            }
+        });
     }
 }
